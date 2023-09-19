2e6a39a3216497bd84b33c003085d7af44331b9a
Thomas8335 <twalton2@css.edu>
2023-09-19 13:21:52 -0500

    Final
    Final

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const blogRoutes = require('./routes/blogRoutes');
+
+// express app
+const app = express();
+
+//Database Connection
+const dbURI = 'Omitted For Security';
+mongoose.connect(dbURI)
+  .then((result) => app.listen(3000))
+  .catch((err)=> console.log(err))
+// listen for requests
+
+
+// register view engine
+app.set('view engine', 'ejs');
+
+// middleware & static files
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(morgan('dev'));
+
+
+
+ app.use((req, res, next) => {
+   res.locals.path = req.path;
+   next();
+ });
+
+app.get('/', (req, res) => {
+  res.redirect('/blogs');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
+});
+
+//blog routes
+app.use('/blogs', blogRoutes);
+
+
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' });
+});
